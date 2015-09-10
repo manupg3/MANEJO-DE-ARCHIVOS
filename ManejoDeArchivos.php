3e0c2fc9303718d826d3e42edf27f9badbceddf0
PeñaManuel <man_uh_14@hotmail.com>
2015-09-10 16:15:07 -0300

    Escritura en archivo/Fwrite

--- ManejoDeArchivos.php
+++ ManejoDeArchivos.php
@@ -6,9 +6,17 @@
 	<?php
 		 $archivo=fopen("TXT/archivo.txt","r");
 		  echo fread($archivo, filesize("TXT/archivo.txt"));                                         
-
-		fclose($archivo);
-		
+         
+		 fclose($archivo);
+     
+		  $archivo=fopen("TXT/archivo.txt","w+");
+		   $esc=fwrite($archivo,"LUGAR DE NACIMIENTO: ADROGUE");
+		   if($esc > 0)
+		{
+			echo "SE PUDO ESCRIBIR EN EL ARCHIVO<br/>";			
+		}
+		 fclose($archivo);
 		?>
+		 
 	</body>
 </html>
